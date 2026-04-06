9e97224bb58ce3ab9cfa443f42b6dd2acb5f6e09
VAINA <vaibhavanil.nagwani@radiometer.dk>
2026-04-06 02:54:46 +0200

    Change prompt from hardcoded to accepting CLI prompt

--- core_ai_agents.py
+++ core_ai_agents.py
@@ -1,3 +1,5 @@
+import sys
+
 from dotenv import load_dotenv
 import os
 from google import genai
@@ -7,12 +9,18 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("Please enter a prompt, or try 'Hey Agent, I want to learn something cool!'")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
     response = client.models.generate_content(
         model="gemini-3.1-flash-lite-preview",
-        contents='''Hi Gemini! I hope you're having a good time!'''
+        contents=prompt
     )
 
-    print(response)
+    print(response.text)
     if response is None or response.usage_metadata is None:
         return
     print(f"Request tokens: {response.usage_metadata.prompt_token_count}")
